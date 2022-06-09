e0fa15c39789d14ae1365b46b61a1480c1435b40
mateo <mateuszka94@gmail.com>
2022-06-09 14:52:00 +0200

    remove server data

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -1,14 +1,34 @@
+using System;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.ResourceLocations;
+using UnityEngine.UIElements;
 
 public class Loader : MonoBehaviour
 {
     private async void Start()
     {
+        DontDestroyOnLoad(gameObject);
         Addressables.InternalIdTransformFunc += OnInternalIdTransfrom;
-        await Addressables.LoadSceneAsync("Default");
+        await Addressables.InstantiateAsync("Cube");
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            LoadScene("Dynamic").Forget();
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            LoadScene("Static").Forget();
+        }
+    }
+
+    private async UniTask LoadScene(string key)
+    {
+        await Addressables.LoadSceneAsync(key);
     }
     
     private string OnInternalIdTransfrom(IResourceLocation arg)
